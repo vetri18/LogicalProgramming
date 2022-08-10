fa0b66027a799b385087de6aa34c030c859bdaaf
vetrivel <vetrivel.kce@gmail.com>
2022-08-10 07:45:31 +0530

    welcome to Logical problems

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace LogicalProgramming
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming");
+        }
+    }
+}
